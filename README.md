274ed4203fecb16da16f695812fb3a1fd1a99f5a
luys <sanchezluys@gmail.com>
2024-04-26 14:53:12 -0500

    se crean las clases tarjeta de credito y compra

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Reto
+Reto Java Listas y Colecciones
+
+1. Crear Aplicacion
+2. Se inicializa la tarjeta de credito con un monto
+3. El limite no se puede superar
+4. Debe existir un menu de compras
+5. Al final se debe mostrar la lista de articulos comprados ordenados de menor a mayor por precio
+
+
---
+++ src/Compra.java
@@ -0,0 +1,23 @@
+public class Compra {
+    private double valor;
+    private String descripcion;
+
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Compra Descripcion: " + descripcion + " valor: " + valor;
+        return s;
+    }
+}
